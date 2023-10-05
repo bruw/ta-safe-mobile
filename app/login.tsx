49a171686b6c4a24f680eac8519a1227d31c1668
Bruno <bruno@pop-os.localdomain>
2023-10-05 18:19:12 -0300

    work in progress

--- app/login.tsx
+++ app/login.tsx
@@ -1,21 +1,57 @@
 import { View, Text, Button } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Stack, useRouter } from "expo-router";
 import useToken from "../states/useToken";
+import api from "../services/api";
+
+
+// "message": "Essas credenciais não foram encontradas em nossos registros.",
+// "errors": {
+//   "email": [
+//     "Essas credenciais não foram encontradas em nossos registros."
+//   ]
 
 export default function _Screen() {
   const router = useRouter();
   const { setToken } = useToken();
+  const [errors, setErrors] = useState<{message:string, errors:{
+    [error: string]: string[]
+  }}>();
 
   const handleLogin = async () => {
-    setToken("ABC");
-    router.push("/(auth)/home");
+    try {
+      const response = await api.post("/api/login", {
+        email: "user1@email.com",
+        password: "1234567",
+      });
+
+      setToken(response.data.token);
+
+      router.push("/(auth)/home");
+    } catch (error: any) {
+      console.log(JSON.stringify(error.response.data, null, 2));
+    }
   };
 
   return (
     <View>
       <Stack.Screen options={{ headerShown: false }} />
       <Text>login</Text>
+
+      <Text>{errors?.message}</Text>
+
+      {
+        Object.keys(errors?.errors)
+      }
+      
+      {
+        // Object.keys(errors).map(key => {
+        //   const error = errors[key];
+
+        //   error.map(e => <Text>{e}</Text>)
+        // });
+      }
+     
       <Button title="login" onPress={handleLogin} />
     </View>
   );
