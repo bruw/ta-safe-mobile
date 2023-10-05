c6a42722a756f3f8aa38848b6ae5155a10f14bab
Andres <andres.porfirio.tablet@gmail.com>
2023-10-05 18:41:12 -0300

    added error handling for login request

--- app/login.tsx
+++ app/login.tsx
@@ -3,55 +3,58 @@ import React, { useState } from "react";
 import { Stack, useRouter } from "expo-router";
 import useToken from "../states/useToken";
 import api from "../services/api";
-
-
-// "message": "Essas credenciais não foram encontradas em nossos registros.",
-// "errors": {
-//   "email": [
-//     "Essas credenciais não foram encontradas em nossos registros."
-//   ]
+import { TextInput } from "react-native-gesture-handler";
 
 export default function _Screen() {
   const router = useRouter();
   const { setToken } = useToken();
-  const [errors, setErrors] = useState<{message:string, errors:{
-    [error: string]: string[]
-  }}>();
+  const [errors, setErrors] = useState<{
+    message: string;
+    errors: {
+      [key: string]: string[];
+    };
+  }>();
 
   const handleLogin = async () => {
     try {
       const response = await api.post("/api/login", {
         email: "user1@email.com",
-        password: "1234567",
+        password: "12345678s",
       });
 
       setToken(response.data.token);
 
       router.push("/(auth)/home");
     } catch (error: any) {
-      console.log(JSON.stringify(error.response.data, null, 2));
+      // console.log(JSON.stringify(error.response.data, null, 2));
+      setErrors(error.response.data);
     }
   };
+
+  console.log(errors?.errors.email.map((e) => e));
 
   return (
     <View>
       <Stack.Screen options={{ headerShown: false }} />
       <Text>login</Text>
 
-      <Text>{errors?.message}</Text>
+      {errors?.message && (
+        <Text style={{ backgroundColor: "red" }}>{errors.message}</Text>
+      )}
 
-      {
-        Object.keys(errors?.errors)
-      }
-      
-      {
-        // Object.keys(errors).map(key => {
-        //   const error = errors[key];
+      {/* <LaravelMessageError error={errors?.message} /> */}
 
-        //   error.map(e => <Text>{e}</Text>)
-        // });
-      }
-     
+      <TextInput
+        style={{ width: "100%", height: 32, backgroundColor: "grey" }}
+      />
+
+      {/* <LaravelAttributeError error={errors?.errors.email} /> */}
+
+      {errors?.errors.email &&
+        errors.errors.email.map((error) => (
+          <Text style={{ fontSize: 10, color: "orange" }}>{error}</Text>
+        ))}
+
       <Button title="login" onPress={handleLogin} />
     </View>
   );
