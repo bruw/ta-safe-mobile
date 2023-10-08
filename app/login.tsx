c81131c2213057759819df23a1cfce00f42944b1
Bruno <bruno@pop-os.localdomain>
2023-10-08 08:16:50 -0300

    created a login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,61 +1,111 @@
-import { View, Text, Button } from "react-native";
 import React, { useState } from "react";
 import { Stack, useRouter } from "expo-router";
-import useToken from "../states/useToken";
+
 import api from "../services/api";
-import { TextInput } from "react-native-gesture-handler";
+import useToken from "../states/useToken";
+import displayErrorsHelper from "../helpers/displayErrors";
+import { UserAfterRegister } from "../types/ApiTypes";
+
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput, Text, Button } from "react-native-paper";
 
 export default function _Screen() {
   const router = useRouter();
   const { setToken } = useToken();
-  const [errors, setErrors] = useState<{
-    message: string;
-    errors: {
-      [key: string]: string[];
-    };
-  }>();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleLogin = async () => {
     try {
-      const response = await api.post("/api/login", {
-        email: "user1@email.com",
-        password: "12345678s",
+      const response = await api.post<UserAfterRegister>("/api/login", {
+        email: email,
+        password: password,
       });
 
       setToken(response.data.token);
 
       router.push("/(auth)/home");
     } catch (error: any) {
-      // console.log(JSON.stringify(error.response.data, null, 2));
-      setErrors(error.response.data);
+      const dataErrors = error.response?.data.errors;
+      displayErrorsHelper(dataErrors);
     }
   };
 
-  console.log(errors?.errors.email.map((e) => e));
-
   return (
-    <View>
+    <SafeAreaView>
       <Stack.Screen options={{ headerShown: false }} />
-      <Text>login</Text>
 
-      {errors?.message && (
-        <Text style={{ backgroundColor: "red" }}>{errors.message}</Text>
-      )}
+      <View style={styles.container}>
+        <View style={styles.loginContainer}>
+          <View style={styles.defaultSpacing}>
+            <Text
+              variant="headlineMedium"
+              style={[styles.loginTitle, styles.text]}
+            >
+              Bem Vindo!
+            </Text>
+            <Text variant="titleMedium" style={styles.text}>
+              Acesse sua conta para continuar.
+            </Text>
+          </View>
 
-      {/* <LaravelMessageError error={errors?.message} /> */}
+          <View>
+            <TextInput
+              label="Email"
+              value={email}
+              onChangeText={(email: string) => setEmail(email)}
+              right={<TextInput.Icon icon="email" />}
+              keyboardType="email-address"
+              style={styles.defaultSpacing}
+            />
 
-      <TextInput
-        style={{ width: "100%", height: 32, backgroundColor: "grey" }}
-      />
+            <TextInput
+              label="Senha"
+              value={password}
+              secureTextEntry
+              right={<TextInput.Icon icon="eye" />}
+              onChangeText={(password: string) => setPassword(password)}
+              style={styles.defaultSpacing}
+            />
 
-      {/* <LaravelAttributeError error={errors?.errors.email} /> */}
-
-      {errors?.errors.email &&
-        errors.errors.email.map((error) => (
-          <Text style={{ fontSize: 10, color: "orange" }}>{error}</Text>
-        ))}
-
-      <Button title="login" onPress={handleLogin} />
-    </View>
+            <Button
+              icon="login"
+              mode="contained"
+              onPress={handleLogin}
+              style={styles.loginButton}
+            >
+              Entrar
+            </Button>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    backgroundColor: "rgba(25, 0, 70, 1)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginContainer: {
+    width: "90%",
+  },
+  defaultSpacing: {
+    marginBottom: 30,
+  },
+  loginTitle: {
+    fontWeight: "900",
+  },
+  text: {
+    color: "#fff",
+  },
+  loginButton: {
+    borderRadius: 6,
+    backgroundColor: "#D35400",
+  },
+});
