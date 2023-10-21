3319bd8d07c7048be72e01b7e0ec18b10699e8e7
Bruno <bruno@pop-os.localdomain>
2023-10-21 08:11:17 -0300

    corrections suggested by lint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Stack } from "expo-router";
+import React from "react";
 import FlashMessage from "react-native-flash-message";
 
 export default function _Layout() {
