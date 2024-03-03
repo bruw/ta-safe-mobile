993d48029ad9726d257f8b5444c84103bce75cbe
bruw <brunojwogt@gmail.com>
2024-03-03 14:38:46 -0300

    UI migration to react native elements

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,30 @@
+import { ThemeProvider, createTheme } from "@rneui/themed";
 import { Stack } from "expo-router";
 import React from "react";
 import FlashMessage from "react-native-flash-message";
 import "../services/lang/translation/i18n";
-import { PaperProvider, DefaultTheme } from "react-native-paper";
 
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
+const theme = createTheme({
+  lightColors: {
     primary: "#6100DD",
-    error: "#D90000",
   },
-};
+  components: {
+    Input: {
+      cursorColor: "#6100DD",
+    },
+    Button: {
+      containerStyle: {
+        borderRadius: 6,
+      },
+    },
+  },
+});
 
 export default function _Layout() {
   return (
-    <PaperProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <Stack />
       <FlashMessage position="top" statusBarHeight={30} />
-    </PaperProvider>
+    </ThemeProvider>
   );
 }
