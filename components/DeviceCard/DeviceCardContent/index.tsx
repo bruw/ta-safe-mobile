a4b1133575d6c47adadee06292b0f941b4c8902b
bruw <brunojwogt@gmail.com>
2024-04-21 14:08:49 -0300

    Date fields added to the device card component

--- components/DeviceCard/DeviceCardContent/index.tsx
+++ components/DeviceCard/DeviceCardContent/index.tsx
@@ -4,6 +4,7 @@ import { Device } from 'types/ApiTypes';
 import { Text } from '@rneui/themed';
 import { stylesDeviceCardContent } from './_styles';
 import { t } from 'i18next';
+import moment from 'moment';
 
 interface DeviceCardContentProps {
     device: Device;
@@ -40,6 +41,20 @@ export default function DeviceCardContent({ device }: DeviceCardContentProps) {
                     {t("components.deviceCard.imei_2")}:{' '}
                 </Text>
                 {device.imei_2}
+            </Text>
+
+            <Text>
+                <Text style={styles.span}>
+                    {t("components.deviceCard.created_at")}:{' '}
+                </Text>
+                {moment(device.created_at).format('DD/MM/YYYY HH:mm')}
+            </Text>
+
+            <Text>
+                <Text style={styles.span}>
+                    {t("components.deviceCard.updated_at")}:{' '}
+                </Text>
+                {moment(device.updated_at).format('DD/MM/YYYY HH:mm')}
             </Text>
         </View>
     );
