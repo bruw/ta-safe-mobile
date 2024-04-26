629bc1943f9f327d0ae973d23701670d868c5003
bruw <brunojwogt@gmail.com>
2024-04-26 19:08:58 -0300

    created custom divider component

--- components/DeviceCards/DeviceBasicInfoCard/index.tsx
+++ components/DeviceCards/DeviceBasicInfoCard/index.tsx
@@ -1,24 +1,21 @@
-import { Divider } from '@rneui/themed';
 import React from 'react';
 import { Device } from 'types/ApiTypes';
 import DeviceCardHeader from './CardHeader';
 import DeviceCardContent from './CardContent';
 import DeviceProvider from 'contexts/DeviceProvider';
 import { Card } from '@rneui/base';
-import { stylesDeviceCard } from './_styles';
+import CustomDivider from 'components/UI/CustomDivider';
 
 interface DeviceCardProps {
     device: Device;
 }
 
 export default function DeviceBasicInfoCard({ device }: DeviceCardProps) {
-    const styles = stylesDeviceCard();
-
     return (
         <DeviceProvider device={device}>
             <Card>
                 <DeviceCardHeader />
-                <Divider style={styles.divider} />
+                <CustomDivider />
                 <DeviceCardContent />
             </Card>
         </DeviceProvider>
