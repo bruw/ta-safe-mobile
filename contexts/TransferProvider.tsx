3f83642a1706417af9230f75873d841803f20de9
bruw <brunojwogt@gmail.com>
2024-05-19 08:09:27 -0300

    modification in TransferProvider to make it possible to update the transfer

--- contexts/TransferProvider.tsx
+++ contexts/TransferProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode } from 'react';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
 import { Transfer } from 'types/ApiTypes';
 
 interface TransferProviderProps {
@@ -8,16 +8,29 @@ interface TransferProviderProps {
 
 interface TransferContextType {
     transfer: Transfer;
+    updateTransfer: (updatedTransfer: Transfer) => void;
 }
 
 export const TransferContext = createContext<TransferContextType>({
     transfer: {} as Transfer,
+    updateTransfer: () => { },
 });
 
 export default function TransferProvider({ transfer, children }: TransferProviderProps) {
+    const [currentTransfer, setCurrentTransfer] = useState<Transfer>(transfer);
+
+    useEffect(() => {
+        setCurrentTransfer(transfer);
+    }, [transfer]);
+
+    const updateTransfer = (updatedTransfer: Transfer) => {
+        setCurrentTransfer(updatedTransfer);
+    };
+
     return (
         <TransferContext.Provider value={{
-            transfer: transfer
+            transfer: currentTransfer,
+            updateTransfer,
         }}>
             {children}
         </TransferContext.Provider>
