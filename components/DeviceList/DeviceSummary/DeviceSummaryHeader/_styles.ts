0ec7ddd738830895ad44927d8d2dfd94826eb5b2
bruw <brunojwogt@gmail.com>
2024-05-20 20:34:03 -0300

    excluded BadgeStatus

--- components/DeviceList/DeviceSummary/DeviceSummaryHeader/_styles.ts
+++ components/DeviceList/DeviceSummary/DeviceSummaryHeader/_styles.ts
@@ -1,21 +1,7 @@
 import { makeStyles } from "@rneui/themed";
 
 export const stylesDeviceSummaryHeader = makeStyles(() => ({
-    container: {
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-    },
     title: {
-        width: "40%",
-        alignItems: "flex-start",
-    },
-    textTitle: {
         fontWeight: "900",
     },
-    status: {
-        width: "40%",
-        alignItems: "flex-end"
-    }
 }));
--- components/DeviceList/DeviceSummary/DeviceSummaryHeader/index.tsx
+++ components/DeviceList/DeviceSummary/DeviceSummaryHeader/index.tsx
@@ -1,24 +1,15 @@
 import { Text } from "@rneui/themed";
-import { View } from "react-native";
 import { stylesDeviceSummaryHeader } from "./_styles";
 import { useContext } from "react";
 import { DeviceContext } from "contexts/DeviceProvider";
-import BadgeStatus from "components/UI/BadgeStatus";
 
 export default function DeviceSummaryHeader() {
     const styles = stylesDeviceSummaryHeader();
     const { device } = useContext(DeviceContext);
 
     return (
-        <View style={styles.container}>
-            <View style={styles.title}>
-                <Text style={styles.textTitle}>
-                    {device.device_model.name}
-                </Text>
-            </View>
-            <View style={styles.status}>
-                <BadgeStatus status={device.validation_status} />
-            </View>
-        </View>
+        <Text style={styles.title}>
+            {device.device_model.name}
+        </Text>
     );
 }
